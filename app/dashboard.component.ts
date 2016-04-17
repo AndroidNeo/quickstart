846dbc911c7c445e6571dde0f5c3432776f5a752
almejro <almejroz@gmail.com>
2016-04-17 17:23:41 +0300

    DASHBOARD TOP HEROES

---
+++ app/dashboard.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by artur on 17.04.16.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-dashboard',
+    template: '<h3>My Dashboard</h3>'
+})
+
+export class DashboardComponent {
+    
+}
